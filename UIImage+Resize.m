59c516863ca35ef1bca49a474aaab8364ef9dcb4
emil.wojtaszek <emil.wojtaszek@gmail.com>
2012-09-28 15:53:57 +0200

    move comments to .h

--- UIImage+Resize.m
+++ UIImage+Resize.m
@@ -9,9 +9,7 @@
 
 @implementation UIImage (Resize)
 
-// Returns a copy of this image that is cropped to the given bounds.
-// The bounds will be adjusted using CGRectIntegral.
-// This method ignores the image's imageOrientation setting.
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIImage *)croppedImage:(CGRect)bounds {
     CGImageRef imageRef = CGImageCreateWithImageInRect([self CGImage], bounds);
     UIImage *croppedImage = [UIImage imageWithCGImage:imageRef];
@@ -19,8 +17,7 @@
     return croppedImage;
 }
 
-// Returns a copy of this image that is squared to the thumbnail size.
-// If transparentBorder is non-zero, a transparent border of the given size will be added around the edges of the thumbnail. (Adding a transparent border of at least one pixel in size has the side-effect of antialiasing the edges of the image when rotating it using Core Animation.)
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIImage *)thumbnailImage:(NSInteger)thumbnailSize
           transparentBorder:(NSUInteger)borderSize
                cornerRadius:(NSUInteger)cornerRadius
@@ -43,8 +40,7 @@
     return [transparentBorderImage roundedCornerImage:cornerRadius borderSize:borderSize];
 }
 
-// Returns a rescaled copy of the image, taking into account its orientation
-// The image will be scaled disproportionately if necessary to fit the bounds specified by the parameter
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIImage *)resizedImage:(CGSize)newSize interpolationQuality:(CGInterpolationQuality)quality {
     BOOL drawTransposed;
     CGAffineTransform transform = CGAffineTransformIdentity;
@@ -76,7 +72,7 @@
     return [self resizedImage:newSize transform:transform drawTransposed:drawTransposed interpolationQuality:quality];
 }
 
-// Resizes the image according to the given content mode, taking into account the image's orientation
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIImage *)resizedImageWithContentMode:(UIViewContentMode)contentMode
                                   bounds:(CGSize)bounds
                     interpolationQuality:(CGInterpolationQuality)quality {
@@ -102,12 +98,10 @@
     return [self resizedImage:newSize interpolationQuality:quality];
 }
 
-#pragma mark -
-#pragma mark Private helper methods
 
-// Returns a copy of the image that has been transformed using the given affine transform and scaled to the new size
-// The new image's orientation will be UIImageOrientationUp, regardless of the current image's orientation
-// If the new size is not integral, it will be rounded up
+#pragma mark - Private helper methods
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIImage *)resizedImage:(CGSize)newSize
                 transform:(CGAffineTransform)transform
            drawTransposed:(BOOL)transpose
@@ -149,7 +143,7 @@
     return newImage;
 }
 
-// Returns an affine transform that takes into account the image orientation when drawing a scaled image
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (CGAffineTransform)transformForOrientation:(CGSize)newSize {
     CGAffineTransform transform = CGAffineTransformIdentity;
     
